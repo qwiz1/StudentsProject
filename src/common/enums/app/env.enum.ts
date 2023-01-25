4d87eb4926127489ad7af02c8b4dbfd29389380d
Andrii Solohub <andrei.sologub12@gmail.com>
2023-01-25 17:13:00 +0200

    + database url

--- src/common/enums/app/env.enum.ts
+++ src/common/enums/app/env.enum.ts
@@ -3,17 +3,15 @@ import { AppEnvironment } from './app-environment.enum';
 
 config();
 
-const { 
-  NODE_ENV, 
-  PORT, 
-  REDIS_URL, 
-  SECRET_KEY 
-} = process.env;
+const { NODE_ENV, PORT, REDIS_URL, SECRET_KEY, DATABASE_URL } = process.env;
 
 const ENV = {
   APP: {
     NODE_ENV: <AppEnvironment>NODE_ENV,
     SERVER_PORT: PORT,
+  },
+  POSTGRES: {
+    URL: DATABASE_URL,
   },
   REDIS: {
     URL: REDIS_URL,
