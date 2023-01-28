4cfef7a465e05df0f2edc195030ea30fb7072e6f
Andrii Solohub <andrei.sologub12@gmail.com>
2023-01-28 21:48:39 +0200

    user payload keys

---
+++ src/common/enums/user/user-payload-key.enum.ts
@@ -0,0 +1,9 @@
+enum UserPayloadKey {
+  USERNAME = 'username',
+  EMAIL = 'email',
+  CLASS_ID = 'class_id',
+  PASSWORD = 'password',
+  PASSWORD_CONFIRMATION = 'password_confirmation'
+}
+
+export { UserPayloadKey };
