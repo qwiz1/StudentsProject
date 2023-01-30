dc220d7955799fca82b35c6f4c69a4641dee8efc
Andrii Solohub <andrei.sologub12@gmail.com>
2023-01-30 22:58:38 +0200

    routes for sign-up & sign-in

--- src/api/auth/auth.api.ts
+++ src/api/auth/auth.api.ts
@@ -1,19 +1,31 @@
 import { Router } from 'express';
 import { ApiPath, AuthApiPath, HttpCode } from '../../common/enums/enums';
+import { auth as authService } from '../../services/services';
+import asyncHandler from 'express-async-handler';
 
 type Args = {
   apiRouter: Router;
+  authService: typeof authService;
 };
 
-const initAuthApi = ({ apiRouter }: Args): Router => {
+const initAuthApi = ({ apiRouter, authService }: Args): Router => {
   const authRouter = Router();
   apiRouter.use(ApiPath.AUTH, authRouter);
 
-  //Just for test
-  authRouter.get(AuthApiPath.ROOT, (req, res) => {
-    res.json('Hey, from auth!').status(HttpCode.OK);
-  });
+  authRouter.post(
+    AuthApiPath.SIGN_UP,
+    asyncHandler(async (req, res) => {
+      const user = await authService.signUp(req.body);
+      res.json(user).status(HttpCode.CREATED);
+    }),
+  );
 
+  authRouter.post(
+    AuthApiPath.SIGN_IN,
+    asyncHandler(async (req, res) => {
+      res.json(await authService.signIn(req.body)).status(HttpCode.OK);
+    }),
+  );
   return authRouter;
 };
 
