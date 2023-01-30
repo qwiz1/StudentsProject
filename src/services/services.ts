dbafda4160a31954e56ceeec17aaa78efd269da4
Andrii Solohub <andrei.sologub12@gmail.com>
2023-01-30 22:57:11 +0200

    initializing auth service and exporting

--- src/services/services.ts
+++ src/services/services.ts
@@ -1,1 +1,9 @@
-export { Socket } from './socket/socket.service'
+import { Auth } from './auth/auth.service';
+import { userRep as userRepository } from '../data/repositories/repositories';
+
+const auth = new Auth({
+  userRepository,
+});
+
+export { Socket } from './socket/socket.service';
+export { auth };
