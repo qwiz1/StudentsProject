397c10b449b63e0296b848c4b546e1ecb4f2430c
Andrii Solohub <andrei.sologub12@gmail.com>
2023-02-01 15:17:35 +0200

    + export enums

--- src/common/enums/enums.ts
+++ src/common/enums/enums.ts
@@ -4,6 +4,10 @@ export {
   UsersApiPath,
   CharactersApiPath,
 } from './api/api';
+export {
+  UserValidationMessage,
+  UserValidationRule,
+} from './validation/validation';
 export { ENV } from './app/app';
 export { HttpCode, ErrorMessage } from './http/http';
 export { UserPayloadKey } from './user/user-payload-key.enum';
