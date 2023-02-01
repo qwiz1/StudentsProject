108f00ce0a25b3b8682119f11feafdfd84ac9a4d
Andrii Solohub <andrei.sologub12@gmail.com>
2023-02-01 15:02:42 +0200

    + get all users & get by id

--- src/data/repositories/user/user.repository.ts
+++ src/data/repositories/user/user.repository.ts
@@ -1,9 +1,25 @@
-import { UserPayloadKey } from '../../../common/enums/user/user-payload-key.enum';
-import { UserCreatePayload } from '../../../common/types/user/user-create-payload.type';
-import { User as TUser } from '../../../common/types/types';
 import { db } from '../../db/db';
+import { UserPayloadKey } from '../../../common/enums/enums';
+import {
+  User as TUser,
+  UserCreatePayload,
+  UserUpdateDTOPayload,
+} from '../../../common/types/types';
 
 class User {
+  public async getAll(): Promise<TUser[]> {
+    //todo: should return users without pass
+    const selectUsersText = `SELECT * FROM users;`;
+    const queryRes = await db.query(selectUsersText);
+    return queryRes.rows;
+  }
+
+  public async getById(id: number): Promise<TUser> {
+    const selectUserText = `SELECT * FROM users WHERE id=$1;`;
+    const queryRes = await db.query(selectUserText, [id]);
+    return queryRes.rows[0];
+  }
+
   public async create(payload: UserCreatePayload): Promise<TUser> {
     const insertUserText = `INSERT INTO users(
       ${UserPayloadKey.USERNAME},
@@ -39,6 +55,22 @@ class User {
     const selectPassText = `SELECT ${UserPayloadKey.PASSWORD} FROM users WHERE email=$1;`;
     const queryRes = await db.query(selectPassText, [email]);
     return queryRes.rows[0].password;
+  }
+
+  public async update(id: number, payload: UserUpdateDTOPayload): Promise<TUser> {
+    //todo: need to change update_at also
+    const updateUserText = `UPDATE users SET
+      ${UserPayloadKey.USERNAME}=$2,
+      ${UserPayloadKey.PASSWORD}=$3,
+      ${UserPayloadKey.CLASS_ID}=$4
+      WHERE id=$1 RETURNING *;`;
+    const queryRes = await db.query(updateUserText, [
+      id,
+      payload.username,
+      payload.password,
+      payload.class_id,
+    ]);
+    return queryRes.rows[0];
   }
 }
 
