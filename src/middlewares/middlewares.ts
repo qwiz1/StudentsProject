5ea81d58c04d2aeb3b706d8103d5ecca9e266442
Andrii Solohub <andrei.sologub12@gmail.com>
2023-02-01 15:30:53 +0200

    + export validate schema middleware

--- src/middlewares/middlewares.ts
+++ src/middlewares/middlewares.ts
@@ -1,3 +1,4 @@
 export { checkJwt } from './jwt/jwt.middleware';
 export { logRequest } from './log-request/log-request.middleware';
 export { handleError } from './handle-error/handle-error.middleware';
+export { validateSchema } from './validation/validation.middleware';
