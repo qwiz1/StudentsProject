040e74e3cda33ce56d9a77772490b4538de8aa9b
Andrii Solohub <andrei.sologub12@gmail.com>
2023-02-01 15:20:10 +0200

    export user types

--- src/common/types/user/user.ts
+++ src/common/types/user/user.ts
@@ -1,2 +1,4 @@
 export type { User } from './user.type';
 export type { UserCreatePayload } from './user-create-payload.type';
+export type { UserUpdatePayload } from './user-update-payload.type';
+export type { UserUpdateDTOPayload } from './user-update-dto-payload.type';
