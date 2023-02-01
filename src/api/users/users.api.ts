717a735f1a840feaa4536a934ae7ee6d849e27ba
Andrii Solohub <andrei.sologub12@gmail.com>
2023-02-01 15:43:16 +0200

    routes for user update and gets methods

--- src/api/users/users.api.ts
+++ src/api/users/users.api.ts
@@ -1,30 +1,52 @@
 import { Router } from 'express';
 import { ApiPath, UsersApiPath, HttpCode } from '../../common/enums/enums';
-import { usersMock } from './users.mock';
+import asyncHandler from 'express-async-handler';
+import { user as userService } from '../../services/services';
+import {
+  checkJwt as checkJwtMiddleware,
+  validateSchema as validateSchemaMiddleware,
+} from '../../middlewares/middlewares';
+import { userUpdate as userUpdateValidationSchema } from '../../validation-schemas/validation-schemas';
 
 type Args = {
   apiRouter: Router;
+  userService: typeof userService;
 };
 
-const initUsersApi = ({ apiRouter }: Args): Router => {
+const initUsersApi = ({ apiRouter, userService }: Args): Router => {
   const userRouter = Router();
   apiRouter.use(ApiPath.USERS, userRouter);
 
-  userRouter.get(UsersApiPath.ROOT, (req, res) => {
-    //Todo: return res.json(await userService.getAll()).status(HttpCode.OK);
-    return res.json(usersMock).status(HttpCode.OK);
-  });
+  userRouter.get(
+    UsersApiPath.ROOT,
+    checkJwtMiddleware,
+    asyncHandler(async (_req, res) => {
+      res.json(await userService.getAll()).status(HttpCode.OK);
+    }),
+  );
 
-  userRouter.get(UsersApiPath.$ID, (req, res) => {
-    const user = usersMock.find((user) => user.id === req.params.id);
-    //Todo: return res.json(await userService.getById()).status(HttpCode.OK);
-    return res.json(user).status(HttpCode.OK);
-  });
+  userRouter.get(
+    UsersApiPath.$ID,
+    //todo: check (userOwner)? get user: throw error
+    checkJwtMiddleware,
+    asyncHandler(async (req, res) => {
+      res
+        .json(await userService.getById(Number(req.params.id)))
+        .status(HttpCode.OK);
+    }),
+  );
 
-  userRouter.put(UsersApiPath.$ID, (req, res) => {
-    //To-do: res.json(await userService.update(Number(req.params.id), req.body)).status(HttpCode.OK);
-    return res.json('Not implemented yet').status(HttpCode.OK)
-  });
+  userRouter.put(
+    UsersApiPath.$ID,
+    //todo: check (userOwner)? edit user: throw error
+    checkJwtMiddleware,
+    validateSchemaMiddleware(userUpdateValidationSchema),
+    asyncHandler(async (req, res) => {
+      res
+        .json(await userService.update(Number(req.params.id), req.body))
+        .status(HttpCode.OK);
+    }),
+  );
 
   return userRouter;
 };
