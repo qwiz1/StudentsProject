695747100079be24cdc30d47e91eba0f407e1c3b
Andrii Solohub <andrei.sologub12@gmail.com>
2023-02-01 15:12:22 +0200

    + error message

--- src/common/enums/http/error-mesage.enum.ts
+++ src/common/enums/http/error-mesage.enum.ts
@@ -5,6 +5,7 @@ enum ErrorMessage {
   USER_NOT_FOUND = 'User not found',
   WRONG_PASSWORD = 'Wrong password',
   PASSWORDS_NOT_MATCH = 'Passwords do not match',
+  OLD_PASSWORDS_NOT_MATCH = 'Old passwords do not match',
   UNAUTHORIZED_TOKEN = 'No token provided',
   BAD_TOKEN = 'Token is invalid',
   EMAIL_IS_ALREADY_TAKEN = 'Email is already taken',
