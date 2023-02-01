9b95fec4487eef88bd48340ed86a92d495da98b0
Andrii Solohub <andrei.sologub12@gmail.com>
2023-02-01 15:08:49 +0200

    export user service instance

--- src/services/services.ts
+++ src/services/services.ts
@@ -1,9 +1,14 @@
 import { Auth } from './auth/auth.service';
+import { User } from './user/user.service';
 import { userRep as userRepository } from '../data/repositories/repositories';
 
 const auth = new Auth({
   userRepository,
 });
 
+const user = new User({
+  userRepository,
+});
+
 export { Socket } from './socket/socket.service';
-export { auth };
+export { auth, user };
