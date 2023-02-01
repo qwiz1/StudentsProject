dc54a2c4dfe93625b65500b06a82068974cae614
Andrii Solohub <andrei.sologub12@gmail.com>
2023-02-01 15:33:01 +0200

    + using validate schemas for auth sign-in sign-up

--- src/api/auth/auth.api.ts
+++ src/api/auth/auth.api.ts
@@ -2,6 +2,11 @@ import { Router } from 'express';
 import { ApiPath, AuthApiPath, HttpCode } from '../../common/enums/enums';
 import { auth as authService } from '../../services/services';
 import asyncHandler from 'express-async-handler';
+import { validateSchema } from '../../middlewares/validation/validation.middleware';
+import {
+  signUp as signUpValidationSchema,
+  signIn as signInValidationSchema,
+} from '../../validation-schemas/user/user';
 
 type Args = {
   apiRouter: Router;
@@ -14,6 +19,7 @@ const initAuthApi = ({ apiRouter, authService }: Args): Router => {
 
   authRouter.post(
     AuthApiPath.SIGN_UP,
+    validateSchema(signUpValidationSchema),
     asyncHandler(async (req, res) => {
       const user = await authService.signUp(req.body);
       res.json(user).status(HttpCode.CREATED);
@@ -22,6 +28,7 @@ const initAuthApi = ({ apiRouter, authService }: Args): Router => {
 
   authRouter.post(
     AuthApiPath.SIGN_IN,
+    validateSchema(signInValidationSchema),
     asyncHandler(async (req, res) => {
       res.json(await authService.signIn(req.body)).status(HttpCode.OK);
     }),
